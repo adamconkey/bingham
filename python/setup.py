a7138987f74609522799258b1240db5d1ab64560
Adam Conkey <adam.conkey@utah.edu>
2022-03-22 10:08:02 -0600

    Clean up extensions and add python3 language level

--- python/setup.py
+++ python/setup.py
@@ -3,16 +3,18 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
-bingham_extension = Extension(
-    name="pybingham",
-    sources=["bingham_c.pyx"],
-    libraries=["bingham"],
-    include_dirs=[numpy.get_include()],
-    extra_compile_args=["-w"]
-)
+extensions = [
+    Extension(
+        name="pybingham",
+        sources=["bingham_c.pyx"],
+        libraries=["bingham"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=["-w"]
+    )
+]
 
 setup(
     name="pybingham",
-    ext_modules=cythonize([bingham_extension])
+    ext_modules=cythonize(extensions, language_level="3")
 )
 
